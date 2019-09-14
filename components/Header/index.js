342bffe52b1b23be5877bb209c434286870b837d
Freddie <fthompson803@live.com>
2019-09-13 23:17:24 -0400

    adding mvp requirements and playing with stretch

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+let mainHeader = document.querySelector('.header-container');
+function Header() {
+  //creating html elements
+  let header = document.createElement('div');
+  let spanDate = document.createElement('span');
+  let h1 = document.createElement('h1');
+  let spanTemp = document.createElement('span');
+
+  //adding classes to elements
+  header.classList.add('header');
+  spanDate.classList.add('date');
+  spanTemp.classList.add('temp');
+
+  //appending elements to header div
+  header.appendChild(spanDate);
+  header.appendChild(h1);
+  header.appendChild(spanTemp);
+
+  //static content
+  spanDate.textContent = 'March 28, 2019';
+  h1.textContent = 'Lambda Times';
+  spanTemp.textContent = '98' + String.fromCharCode(176);
+
+  return header;
+}
+
+mainHeader.appendChild(Header());
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function makeTab(topic) {
+  //create elements
+  let tab = document.createElement('div');
+  //add class
+  tab.classList.add('tab');
+  tab.textContent = topic;
+  return tab;
+}
+
+makeEachTopic = async url => {
+  try {
+    let res1 = await axios.get(url);
+    //console.log(res1);
+    res1.data.topics.forEach(element => {
+      tabsSpan.appendChild(makeTab(element));
+    });
+    return res1;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+let tabsSpan = document.querySelector('.topics');
+makeEachTopic('https://lambda-times-backend.herokuapp.com/topics');
